4935efc96b9a16d4b2f8011c548b50f490fe169e
JaeWon Jin <j1jin286@gmail.com>
2023-10-03 17:46:16 -0700

    Completed Bernie’s Beautiful Bikes

--- bikes.sql
+++ bikes.sql
@@ -3,3 +3,32 @@ CREATE TABLE customers (
     first_name VARCHAR(25) NOT NULL,
     last_name VARCHAR(25) NOT NULL
 );
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    order_timestamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    customer_id INT NOT NULL REFERENCES customers
+);
+
+CREATE TABLE models (
+    code TEXT PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    base_price NUMERIC(10,2) NOT NULL
+);
+-- Order items/ different name to make clear
+-- NULL means unknown, not empty
+CREATE TABLE models_orders (
+    id SERIAL PRIMARY KEY,
+    price NUMERIC(10,2) NOT NULL,
+    note TEXT NOT NULL,
+    model_id TEXT NOT NULL REFERENCES models,
+    order_id INT NOT NULL REFERENCES orders
+);
+
+INSERT INTO models
+(code, name, base_price)
+VALUES
+('FLY', 'The Flyer', 500),
+('HAUL', 'The Big Haul', 600),
+('SKIN', 'The Skinny', 700),
+('BUD', 'Big Buddy', 800);
